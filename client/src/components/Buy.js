8b5f4e879ad73e5c6b54b63e8ff804fbe1ad5727
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2022-12-28 23:58:39 +0530

    added tailwindcss and made changes to the file

--- client/src/components/Buy.js
+++ client/src/components/Buy.js
@@ -14,40 +14,6 @@ const Buy = ({ state }) => {
         console.log("transaction is done");
     };
 
-  // return (
-  //     <>
-  //         <div className="container-md" style={{ width: "50%", marginTop: "25px" }}>
-  //             <form onSubmit={buyChai}>
-  //             <div className="mb-3">
-  //                 <label className="form-label">Name</label>
-  //                 <input
-  //                 type="text"
-  //                 className="form-control"
-  //                 id="name"
-  //                 placeholder="Enter Your Name"
-  //                 />
-  //             </div>
-  //             <div className="mb-3">
-  //                 <label className="form-label">Message</label>
-  //                 <input
-  //                 type="text"
-  //                 className="form-control"
-  //                 id="message"
-  //                 placeholder="Enter Your Message"
-  //                 />
-  //             </div>
-  //             <button
-  //                 type="submit"
-  //                 className="btn btn-primary"
-  //                 disabled={!state.contract}
-  //             >
-  //                 Pay
-  //             </button>
-  //             </form>
-  //         </div>
-  //     </>
-  // );
-
     return (
         <>
         <section className=" mx-auto mt-12 md:mt-20 text-center w-full">
